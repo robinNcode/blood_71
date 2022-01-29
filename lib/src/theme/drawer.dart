eb6e0ddd9dc9e5695282a80d84111a5bd9517bb3
robinNcode <msmrobin518@gmail.com>
2022-01-30 01:55:29 +0600

    drawer on process

--- lib/src/theme/drawer.dart
+++ lib/src/theme/drawer.dart
@@ -10,31 +10,42 @@ class CustomDrawer extends StatelessWidget {
       child: ListView(
         padding: const EdgeInsets.all(0),
         children: [
-          AppBar(
+          /*AppBar(
               backgroundColor: Colors.white,
               leading: BackButton(
-                color: Colors.teal,
+                color: Colors.red,
                 onPressed: () {
                   Navigator.pop(context);
                 },
               ),
               title: const Text(
                 'Menu',
-                style: TextStyle(fontSize: 20, color: Colors.teal),
+                style: TextStyle(fontSize: 20, color: Colors.red),
               )),
+           */
           Padding(
               padding: EdgeInsets.symmetric(
-                  vertical: _size.height / 20, horizontal: _size.width / 10),
+                  vertical: _size.height / 30, horizontal: _size.width / 25),
               child: Column(
                 children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          AssetImage('assets/images/logo.png'),
+
+                        ],
+                      ),
+                    ],
+                  ),
                   InkWell(
                     onTap: () {},
                     child: Row(
                       children: const [
-                        Icon(Icons.info_outline, color: Colors.teal),
+                        Icon(Icons.info_outline, color: Colors.red),
                         Text(
                           '  First Page',
-                          style: TextStyle(color: Colors.teal, fontSize: 22),
+                          style: TextStyle(color: Colors.red, fontSize: 18, fontFamily: 'Roboto'),
                         )
                       ],
                     ),
@@ -43,10 +54,10 @@ class CustomDrawer extends StatelessWidget {
                     onTap: () {},
                     child: Row(
                       children: const [
-                        Icon(Icons.info_outline, color: Colors.teal),
+                        Icon(Icons.info_outline, color: Colors.red),
                         Text(
                           '  Second Page',
-                          style: TextStyle(color: Colors.teal, fontSize: 22),
+                          style: TextStyle(color: Colors.red, fontSize: 18, fontFamily: 'Roboto'),
                         )
                       ],
                     ),
@@ -55,10 +66,10 @@ class CustomDrawer extends StatelessWidget {
                     onTap: () {},
                     child: Row(
                       children: const [
-                        Icon(Icons.info_outline, color: Colors.teal),
+                        Icon(Icons.info_outline, color: Colors.red),
                         Text(
                           '  Third Page',
-                          style: TextStyle(color: Colors.teal, fontSize: 22),
+                          style: TextStyle(color: Colors.red, fontSize: 18, fontFamily: 'Roboto'),
                         )
                       ],
                     ),
