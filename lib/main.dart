0d6807586770cfd8e517df790369affee9c50ae7
sajibAdhi <sajibadhikery@gmail.com>
2022-01-30 00:04:33 +0600

    Sizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:blood_71/src/config/routes.dart';
-import 'package:blood_71/src/theme/loading_page.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Blood Ekattor',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //initialRoute: 'initial',
-      //routes: Routes.getRoute(),
-      home: const LoadingPage(),
-    );
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Blood Ekatoor',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: 'home',
+        routes: Routes.getRoute(),
+      );
+    });
   }
 }
